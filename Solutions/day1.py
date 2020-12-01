fe24b16a8ad01da034b3e68bb8c7667f4d2ecb69
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-12-01 18:29:48 -0500

    slight refactor and day2 prep

--- Solutions/day1.py
+++ Solutions/day1.py
@@ -1,13 +1,18 @@
 def day1():
-    data = [int(x) for x in open(r'C:\Users\Quinn\Documents\GitHub\AdventOfCode2020\Inputs\day1.txt')]
+    data = [int(x) for x in open(r'Inputs\day1.in')]
+    print('Part 1 result: ' + str(part1(data)))
+    print('Part 2 result: ' + str(part2(data)))
+
+def part1(data):
     for x in data:
         if (2020 - x in data):
-            print((2020 - x) * x) 
-            
-    for i in data:
-        for j in data:
-            for h in data:
-                if (i + j + h == 2020):
-                    print(i*j*h)
+            return ((2020 - x) * x) 
+
+def part2(data):
+    for x in data:
+        for y in data:
+            for z in data:
+                if (x + y + z == 2020):
+                    return (x*y*z)
 
 day1()
---
+++ Solutions/day2.py
@@ -0,0 +1,11 @@
+def day2():
+    data = [int(x) for x in open(r'Inputs\day1.in')]
+    print('Part 1 result: ' + str(part1(data)))
+    print('Part 2 result: ' + str(part2(data)))
+
+
+part1(data):
+    
+part2(data):
+
+day2()
